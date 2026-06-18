cf9be1bc6e8d620bfbd60e5d08f1503134522ab1
Us <abuumarsharipov15@gmail.com>
2026-06-18 16:40:53 +0500

    use hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useForm} from 'react-hook-form'
 
 const App = () => {
+  const [data,setData]=useState([])
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: {errors}
+  } = useForm()
+  const onSubmit = (value)=>{
+    setData((prev)=>[...prev,value])
+  }
   return (
-    <div>App</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" {...register('name')} />
+        <input type="text" {...register('email', {required:true})} />
+        {errors.email && <p>Error</p>}
+        <input type="submit" />
+      </form>
+
+
+      <div>
+        {data.map((el)=>{
+          return <div>
+            <h1>{el.name}</h1>
+          </div>
+        })}
+      </div>
+
+    </div>
   )
 }
 
